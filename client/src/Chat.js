c944f3ae45e1d70711004539c3b2c35231f0826e
sumedha-95 <sumedhalakmal95@gmail.com>
2023-01-22 23:05:40 +0530

    added chat component

---
+++ client/src/Chat.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function Chat() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>HI</p>
+      </header>
+    </div>
+  );
+}
+
+export default Chat;
